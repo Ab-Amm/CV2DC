93f1a41228397c6c3350ee4c18a923e81794d145
Fatima ait hssaine <aithssaine.fatimazahra@etu.uae.ac.ma>
2025-07-16 14:35:12 +0100

    date

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Forcer l'extension .js pour tous les chunks
+        entryFileNames: '[name]-[hash].js',
+        chunkFileNames: '[name]-[hash].js',
+        assetFileNames: '[name]-[hash].[ext]'
+      }
+    }
+  }
 })
